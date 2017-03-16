767442859da8f30338690bcfced1e5e94008da44
Terpal47 <alexander.terp@gmail.com>
2017-03-16 20:29:51 +1100

    Initial commit for A-Star algorithm addition

---
+++ Algorithms/A-Star Pathfinding/heap.h
@@ -0,0 +1,167 @@
+#define DEBUG_H 0
+
+#define DATATYPE Node
+#define PROPERTY .f_cost
+
+#define MAKE_PROP(var) (var PROPERTY)
+
+typedef struct node {
+    char type;
+    int row;
+    int col;
+
+    int g_cost; // Distance to start node.
+    int h_cost; // Distance to end node.
+    int f_cost; // Sum of g_cost and h_cost.
+
+    int is_open;
+    int analyzed_once;
+    int num_neighbors;
+    int heap_index;
+    struct node* prev_node;
+} Node;
+
+// Function declarations --------------------------------------------------------------------------
+
+void heapify(Node** array, int len, int (*cmp)(Node* a, Node* b));
+Node* pop_root(Node** arr, int len, int (*cmp)(Node* a, Node* b));
+void add_node ( Node* node, Node** array, int len, int (*cmp)(Node* a, Node* b) );
+int get_parent_index(int child_index);
+int get_first_child_index(int parent_index);
+void check_node( Node** array, int len, int index, int (*cmp)(Node* a, Node* b) );
+void swap_nodes(Node** num1, Node** num2);
+// int min(Node* a, Node* b);
+// int max(Node* a, Node* b);
+
+// Function declarations end ----------------------------------------------------------------------
+
+void heapify(Node** array, int len, int (*cmp)(Node* a, Node* b)) {
+    /* Turns any given array of integers into a heap structure following the rules given by the
+    given cmp function. */
+
+    // Get the index of the first parent node.
+    int first_parent_index = get_parent_index(len - 1);
+
+    // Check that each parent node is in the right position relative to its children.
+    // This guarentees a heap structure.
+    for (int i = first_parent_index; i >= 0; i--) {
+        check_node(array, len, i, cmp);
+    }
+}
+
+Node* pop_root(Node** arr, int len, int (*cmp)(Node* a, Node* b)) {
+    /* Removes the root of the binary tree and returns it. Reorganizes the array back into heap
+    form afterwards. */
+
+    // Save the root value and swap it with the last leaf. Then free the memory of the last leaf.
+    Node* root = arr[0];
+    if (DEBUG_H) printf("Pop ");
+    swap_nodes(&arr[0], &arr[--len]);
+
+    // Reorganize the array back into a correct heap structure and return the former root.
+    check_node(arr, len, 0, cmp);
+    return root;
+}
+
+void add_node ( Node* node, Node** array, int len, int (*cmp)(Node* a, Node* b) ) {
+    /* Adds a given node to the heap and restructures the heap into proper heap structure following
+    the rules described by the given cmp function. */
+
+    if (DEBUG_H) printf("Add node %d\n", node->f_cost);
+
+    // Allocate memory for new node and insert it at the end.
+    array[len-1] = node;
+    
+    // Reorganize the array until it is a proper heap structure following the rules described by
+    // the given cmp function.
+    int parent_index, index = len-1;
+    while (1) {
+        // Repeatedly compare the leaf with its parent. If out of position, swap them and check the
+        // node's new parent (unless the node is now the root).
+
+        parent_index = get_parent_index(index);
+        if (parent_index == 0) {
+            // The node is at the root. No more parents to check. Must be in correct position.
+            node->heap_index = index;
+            break;
+        }
+
+        if (cmp(array[index], array[parent_index])) {
+            // Node must be out of position relative to its parent.
+            swap_nodes(&array[index], &array[parent_index]);
+            index = parent_index;
+        } else {
+            // Node must be in the correct position relative to its parents. Array must be in
+            // proper heap structure now.
+            node->heap_index = index;
+            break;
+        }
+
+        
+    }
+}
+
+int get_parent_index(int child_index) {
+    // Given the index of an array, returns the index of its parent. If it does
+    // not have a parent (i.e. its the root), it will return its own index. This
+    // is dealt with uniquely in the functions that call this one.
+
+    return (child_index - 1)  / 2;
+}
+
+int get_first_child_index(int parent_index) {
+    // Given the index of an array, returns the index of its parent. If it does
+    // not have a parent (i.e. its the root), it will return its own index. This
+    // is dealt with uniquely in the functions that call this one.
+
+    if (DEBUG_H) printf("Parent_i %d -> Child_i %d\n", parent_index, 2 * parent_index + 1);
+    return (2 * parent_index + 1);
+}
+
+void check_node( Node** array, int len, int index, int (*cmp)(Node* a, Node* b) ) {
+    /* Given the index of an array, checks whether its in the correct heap 
+    position relative to its children according to the rules of the given cmp 
+    function. If not, switches with one of the children and recursively calls 
+    this function on the node at the new position. */
+
+    // Determine the number of children that the nodes has and save their 
+    // indices in an array.
+    int children = 0;
+    int children_index_arr[2];
+    children += ( children_index_arr[0] = get_first_child_index(index) ) <= (len - 1);
+    children += ( children_index_arr[1] = get_first_child_index(index) + 1 ) <= (len - 1);
+    if (DEBUG_H) printf("Checknode! Index: %d | Number: %d | Children: %d\n", index, array[index]->f_cost, children);
+
+    // Determine which child to swap with to fulfill the heap structure and swap
+    // if necessary. TODO: COMMENT
+    if (children == 1) {
+        if (cmp(array[children_index_arr[0]], array[index])) {
+            swap_nodes(&array[index], &array[children_index_arr[0]]);
+            check_node(array, len, children_index_arr[0], cmp);
+        } else if (DEBUG_H) {
+            printf("Correct order, no swap.\n");
+        }
+    } else if (children == 2) {
+        int important_child_index;
+        int cmp_result = cmp(array[children_index_arr[0]], array[children_index_arr[1]]);
+        if (cmp_result == 1) {
+            important_child_index = children_index_arr[0];
+        } else {
+            important_child_index = children_index_arr[1];
+        }
+
+        if (cmp(array[important_child_index], array[index])) {        
+            swap_nodes(&array[index], &array[important_child_index]);
+            check_node(array, len, important_child_index, cmp);
+        }
+    } else if (DEBUG_H) {
+        printf("Exit check_node, no children.\n");
+    }
+}
+
+void swap_nodes(Node** num1, Node** num2) {
+    if (DEBUG_H) printf("Swap!\n");
+    Node* temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
---
+++ Algorithms/A-Star Pathfinding/main.c
@@ -0,0 +1,307 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "heap.h"
+
+#define START 'S'
+#define END 'E'
+#define OBSTACLE 'Z'
+#define EMPTY '_'
+#define NEW_LINE '\n'
+#define PATH_CHAR '.'
+
+// The number of neighbors surrounding a node usually (except edge and corner
+// nodes).
+#define NUM_SURR 8
+#define INIT_HEAP_SIZE 16
+
+#define PRECISION_MULT 10
+#define SQRT_2 (int) (PRECISION_MULT * 1.4142)
+
+#define DEBUG 0
+
+// Define global variables.
+int NUM_ROWS;
+int NUM_COLS;
+Node* start_node;
+Node* end_node;
+
+typedef struct {
+    Node** nodes;
+    int curr_size;
+    int num_open_nodes;
+} Heap;
+
+Node*** load_map(char *map_name, Heap* open_nodes);
+Node** get_neighbors(Node*** grid, Node* node);
+int analyze_step(Node*** grid, Node* node, Heap* open_nodes);
+int analyze_node(Node* next_node, Node* curr_node);
+int get_distance(Node* node1, Node* node2);
+int cmp(Node* a, Node* b);
+int min(int a, int b);
+void print_grid(Node*** grid);
+void print_node_list(Node** list, int len);
+void add_open_node(Node* node, Heap* open_nodes);
+void draw_path(Node*** grid, Node* end_node);
+
+int main(int argc, char *argv[]) {
+
+    // Ensure a map is provided as a command line argument.
+    if (argc != 2) {
+        printf("No argument given. Exiting...\n");
+        return 0;
+    }
+
+    Heap open_nodes;
+
+    open_nodes.curr_size = INIT_HEAP_SIZE;
+    open_nodes.nodes = malloc(open_nodes.curr_size * sizeof 
+        *(open_nodes.nodes) );
+    open_nodes.num_open_nodes = 0;
+
+    Node*** grid = load_map(argv[1], &open_nodes);
+    //print_grid(grid);
+
+    // Node** neighbors = get_neighbors(grid, grid[3][8]);
+    // print_node_list(neighbors, grid[3][8]->num_neighbors);
+
+    // analyze_step(grid, start_node);
+
+    // A STAR
+    int found = 0;
+    while (!found) {
+        found = analyze_step(grid, open_nodes.nodes[0], &open_nodes);
+    }
+
+    //draw_path(grid, end_node);
+    printf("Found!\n");
+
+    return 0;
+}
+
+Node*** load_map(char *map_name, Heap* open_nodes) {
+
+    // Load in the map file.
+    FILE *fp = fopen(map_name, "r");
+
+    // Read the map's dimensions.
+    fscanf(fp, "%dx%d\n\n", &NUM_ROWS, &NUM_COLS);
+
+    // Allocate memory for the grid.
+    Node*** grid = malloc( NUM_ROWS * sizeof *(grid) );
+    for (int i = 0; i < NUM_ROWS; i++) {
+        grid[i] = malloc( NUM_COLS * sizeof *(grid[i]));
+    }
+
+    // Read in map character by character and save it into the grid.
+    char curr_char;
+    int row = 0, col = 0;
+    while ( (curr_char = getc(fp)) != EOF ) {
+
+        if (curr_char == NEW_LINE) {
+            row++;
+            col = 0;
+            continue;
+        }
+
+        // Create and allocate memory for a new node.
+        Node* new_node = malloc(sizeof *(new_node));
+        new_node->type = curr_char;
+        new_node->row  = row;
+        new_node->col  = col;
+
+        // Set the status of the node to open or not (implied closed).
+        if (new_node->type == EMPTY) {
+            new_node->is_open = 1;
+            new_node->analyzed_once = 0;
+            new_node->heap_index = -1;
+            new_node->prev_node = malloc(sizeof *(new_node->prev_node));
+        } else if (new_node->type == END) {
+            new_node->is_open = 1;
+            end_node = new_node;
+            new_node->analyzed_once = 0;
+            new_node->heap_index = -1;
+            new_node->prev_node = malloc(sizeof *(new_node->prev_node));
+        } else if (new_node->type == START) {
+            new_node->is_open = 1;
+            new_node->analyzed_once = 0;
+            new_node->heap_index = -1;
+            new_node->g_cost = 0;
+            start_node = new_node;
+            add_open_node(new_node, open_nodes);
+        } else if (new_node->type == OBSTACLE) {
+            new_node->is_open = 0;
+        }
+
+        grid[row][col] = new_node;
+        if (DEBUG >= 3) printf("Saved %c\n", grid[row][col]->type);
+
+        col++;  
+    }
+
+    if (DEBUG >= 2) printf("load_map function finished.\n");
+    return grid;
+}
+
+Node** get_neighbors(Node*** grid, Node* node) {
+    /* Returns an array of pointers to neighboring open nodes of the 
+    given node. */
+
+    int counter = 0;
+    Node** neighbors = malloc(NUM_SURR * sizeof *(neighbors));
+
+    for (int row = node->row - 1; row <= node->row + 1; row++) {
+        for (int col = node->col - 1; col <= node->col + 1; col++) {
+
+            if ( (row == node->row && col == node->col) ||
+                row < 0 || row >= NUM_ROWS ||
+                col < 0 || col >= NUM_COLS ||
+                !grid[row][col]->is_open) {
+                continue;
+            }
+
+            neighbors[counter++] = grid[row][col];
+        }
+    }
+
+    node->num_neighbors = counter;
+    if (DEBUG == 2) printf("num_neighbors: %d\n", node->num_neighbors);
+    return neighbors;
+}
+
+int analyze_step(Node*** grid, Node* node, Heap* open_nodes) {
+    // TODO IMPLEMENT HEAP
+
+    if (DEBUG) printf("New step: (%d, %d)\n", node->row, node->col);
+    
+    node->is_open = 0;
+
+    pop_root(open_nodes->nodes, (open_nodes->num_open_nodes)--, cmp);
+
+    int found;
+    Node** neighbors = get_neighbors(grid, node);
+
+    for (int i = 0; i < node->num_neighbors; i++) {
+        found = analyze_node(neighbors[i], node);
+
+        if (found) {
+            end_node = neighbors[i];
+            return 1;
+        }
+
+        if (!neighbors[i]->analyzed_once) {
+            add_open_node(neighbors[i], open_nodes);
+            neighbors[i]->analyzed_once = 1;
+        } else {
+            int parent_index = get_parent_index(neighbors[i]->heap_index);
+            check_node(open_nodes->nodes, open_nodes->num_open_nodes, parent_index, cmp);
+        }
+    }
+
+    return 0;
+}
+
+int analyze_node(Node* next_node, Node* curr_node) {
+    /* Given a node "next_node", analyzes and updates its g_cost, h_cost, and f_cost. */
+
+    int g_cost = get_distance(next_node, curr_node) + curr_node->g_cost;
+
+    if (next_node->analyzed_once) {
+        // If the node has already been analyzed, then h_cost will not change.
+        // Just check if g_cost is lower, and if so, update it and f_cost.
+        if (g_cost < next_node->g_cost) {
+            next_node->g_cost = g_cost;
+            next_node->f_cost = next_node->g_cost + next_node->h_cost;
+            next_node->prev_node = curr_node;
+            if (DEBUG) {
+                printf("New path defined: (%d, %d) -> (%d, %d)\n", next_node->prev_node->row, 
+                    next_node->prev_node->col, next_node->row, next_node->col);
+            }
+        } else {
+            return 0;
+        }
+    } else {
+        next_node->g_cost = g_cost;
+        next_node->h_cost = get_distance(next_node, end_node);
+        next_node->f_cost = next_node->g_cost + next_node->h_cost;
+
+        next_node->prev_node = curr_node;
+        if (DEBUG) {
+            printf("New path defined: (%d, %d) -> (%d, %d)*\n", next_node->prev_node->row, 
+                next_node->prev_node->col, next_node->row, next_node->col);
+        }
+    }
+
+    if (DEBUG) {
+        printf("(%d, %d): g_cost %d | h_cost %d | f_cost %d\n", next_node->row, 
+            next_node->col, next_node->g_cost, next_node->h_cost, next_node->f_cost);
+    }
+
+    return (next_node->type == END);
+}
+
+int get_distance(Node* node1, Node* node2) {
+
+    int dx = abs(node1->col - node2->col);
+    int dy = abs(node1->row - node2->row);
+
+    return min(dx, dy) * SQRT_2 + abs(dx - dy) * PRECISION_MULT;
+}
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int cmp(Node* a, Node* b) {
+    if (DEBUG_H) printf("Minning %d and %d -> %d\n", a->f_cost, b->f_cost, (a->f_cost < b->f_cost));
+    if (a->f_cost == b->f_cost) {
+        return (a->h_cost < b->h_cost);
+    } else {
+        return (a->f_cost < b->f_cost);
+    }
+}
+
+// int max(Node a, Node b) {
+//     if (DEBUG_H) printf("Maxing %d and %d -> %d\n", a.f_cost, b.f_cost, (a.f_cost > b.f_cost));
+//     return (a.f_cost > b.f_cost);
+// }
+
+void print_grid(Node*** grid) {
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = 0; j < NUM_COLS; j++) {
+            printf("%c", grid[i][j]->type);
+        }
+        printf("\n");
+    }
+}
+
+void print_node_list(Node** list, int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%c", list[i]->type);
+    }
+    printf("\n");
+}
+
+void add_open_node(Node* node, Heap* open_nodes) {
+    /* Adds a given node to the 'open' heap collection. */
+
+    open_nodes->num_open_nodes++;
+    if (open_nodes->num_open_nodes > open_nodes->curr_size) {
+        open_nodes->curr_size *= 2;
+        open_nodes->nodes = realloc(open_nodes->nodes, open_nodes->curr_size * 
+            sizeof *(open_nodes->nodes) );
+    }
+
+    add_node(node, open_nodes->nodes, open_nodes->num_open_nodes, cmp);
+}
+
+void draw_path(Node*** grid, Node* end_node) {
+
+    Node* curr_node = end_node->prev_node;
+    while (curr_node->type != START) {
+        grid[curr_node->row][curr_node->col]->type = PATH_CHAR;
+        curr_node = curr_node->prev_node;
+    }
+
+    print_grid(grid);
+}
